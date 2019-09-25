86ea48a365f22802dccfc4258d380bed4a8ccfec
Paul OLIVIER <beetwigonline@gmail.com>
2019-09-25 21:36:41 +0200

    pdate with build ant & junit

---
+++ src/promptHello/PromptHello.java
@@ -0,0 +1,7 @@
+package promptHello;
+
+public class PromptHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World Run from JAR");          // the old SysO-statement
+    }
+}
---
+++ test/promptHello/PromptHelloTest.java
@@ -0,0 +1,42 @@
+package promptHello;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PromptHelloTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSucces() {
+		System.out.println("Positive Case Junit");
+		assertTrue(true);
+	}
+	/*
+	@Test
+	public void testFail() {
+		System.out.println("Negative Case Junit");
+		assertTrue(false);
+	}
+	*/
+
+}
